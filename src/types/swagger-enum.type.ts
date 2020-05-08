8ab79733e03651403708547079f48cfe2267f163
William Desportes <williamdes@wdes.fr>
2020-05-08 16:07:40 +0200

    feat: import more files from @nestjs/swagger

---
+++ src/types/swagger-enum.type.ts
@@ -0,0 +1,11 @@
+/**
+ * MIT License
+ * Copyright (c) 2019 Kamil Mysliwiec
+ * @licence https://opensource.org/licenses/MIT
+ * @source https://github.com/nestjs/swagger/blob/4.5.5/lib/types/swagger-enum.type.ts
+ */
+export type SwaggerEnumType =
+    | string[]
+    | number[]
+    | (string | number)[]
+    | Record<number, string>;
