bd80547fd113fef759dfb65ff660c2fd87b7e72d
William Desportes <williamdes@wdes.fr>
2020-05-09 17:39:43 +0200

    chore: split TypeScriptOpenAPIBuilder into a file

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,4 @@
-import { DocumentBuilder } from './scan/document-builder';
-import { OpenAPIObject } from './interfaces';
-import * as fs from 'fs';
-import * as path from 'path';
-import Builders from './builders';
-import Spec from './spec';
-
 export { DocumentBuilder } from './scan/document-builder';
-
-export class TypeScriptOpenAPIBuilder {
-    private static walk(directory: string, callback: (filePath: string) => void): void {
-        fs.readdirSync(directory).forEach(function (name) {
-            var filePath = path.join(directory, name);
-            var stat = fs.statSync(filePath);
-            if (stat.isFile()) {
-                callback(filePath);
-            } else if (stat.isDirectory()) {
-                this.walkSync(filePath, callback);
-            }
-        });
-    }
-
-    private static getAllFilesInDir(directory: string, regex: RegExp): string[] {
-        const files: string[] = [];
-        this.walk(directory, (filePath) => {
-            if (filePath.match(regex)) {
-                files.push(filePath);
-            }
-        });
-        return files;
-    }
-
-    public static buildSpec(document: DocumentBuilder, directory: string, fileRegex: RegExp): OpenAPIObject {
-        const filesToScan = this.getAllFilesInDir(directory, fileRegex);
-        const metas = Builders.buildMetaForFiles(filesToScan);
-        return Spec.buildSpecFromCollectedMeta(metas, document);
-    }
-
-    public static buildJsonSpec(document: DocumentBuilder, directory: string, fileRegex: RegExp): string {
-        return JSON.stringify(this.buildSpec(document, directory, fileRegex));
-    }
-}
+export { TypeScriptOpenAPIBuilder } from './TypeScriptOpenAPIBuilder';
+export * as decorators from './decorators';
+export * as interfaces from './interfaces';
